73ebadaf71af6100c5a3fa307fd99357d5a38e5d
jahazielgpz <166880372+jahazielgp@users.noreply.github.com>
2024-11-04 21:44:56 -0500

    Update acceso.js
    se agregó la funcionalidad de que no se pueda loguear sin ingresar información

--- ihc_tf/Public/Assets/Scripts/acceso.js
+++ ihc_tf/Public/Assets/Scripts/acceso.js
@@ -2,6 +2,21 @@ const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
+const usernameInput = document.querySelector(".sign-in-form input[type='text']");
+const passwordInput = document.querySelector(".sign-in-form input[type='password']");
+const loginBtn = document.querySelector("#login-btn");
+
+function toggleLoginButton() {
+  if (usernameInput.value.trim() && passwordInput.value.trim()) {
+    loginBtn.disabled = false;
+  } else {
+    loginBtn.disabled = true;
+  }
+}
+
+usernameInput.addEventListener("input", toggleLoginButton);
+passwordInput.addEventListener("input", toggleLoginButton);
+
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
 });
