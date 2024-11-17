2172fd4e4aa83db3aae1d19436ff5ab9c546d760
Alison Arrieta <167279472+alisoft08@users.noreply.github.com>
2024-11-17 08:13:55 -0500

    Update menu.js

--- ihc_tf/Public/Assets/Scripts/menu.js
+++ ihc_tf/Public/Assets/Scripts/menu.js
@@ -1,20 +1,32 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.getElementById('menu-btn');
-const closeBtn = document.getElementById('close-btn');
-
 const darkMode = document.querySelector('.dark-mode');
+
 
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 });
 
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
-});
+
 
 darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode-variables');
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentUser = localStorage.getItem("currentUser");
+
+  if (currentUser) {
+    const usernameDisplay = document.getElementById("username-display");
+    if (usernameDisplay) {
+      usernameDisplay.textContent = currentUser;
+    }
+  } else {
+    alert("No has iniciado sesión.");
+    window.location.href = "acceso.html";
+  }
+});
+
 
